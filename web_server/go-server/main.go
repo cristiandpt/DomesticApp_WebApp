388210080e45550c446d225452dc6fce0c5a1868
Cristian David <cristian.pacheco@correounivalle.edu.co>
2023-11-12 17:31:09 -0500

    Implementing the dummy endpoints, response mocks, and requests handlers

--- web_server/go-server/main.go
+++ web_server/go-server/main.go
@@ -3,9 +3,69 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
+
+func domestikaListHandler(c *gin.Context) {
+	c.JSON(
+		200,
+		gin.H{"message": "Hello pong"},
+	)
+}
+
+func newDomestikHandler(c *gin.Context) {
+	c.JSON(
+		200,
+		gin.H{"message": "Hello pong"},
+	)
+}
+
+func UpdateDomestikaHandler(c *gin.Context) {
+	id := c.Param("id")
+	var recipe string
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+	index := -1
+	for i := 0; i < len(recipe); i++ {
+		if "recipe[i]" == id {
+			index = i
+		}
+	}
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Recipe not found"})
+		return
+	}
+	c.JSON(http.StatusOK, recipe)
+}
+
+func DeleteRecipeHandler(c *gin.Context) {
+	c.JSON(
+		200,
+		gin.H{"message": "Hello pong"},
+	)
+}
+
+func SearchDomestikHandler(c *gin.Context) {
+	//tag := c.Query("tag")
+	c.JSON(
+		200,
+		gin.H{"message": "Hello pong"},
+	)
+}
 
 // main is the entry point of the program.
 func main() {
-    fmt.Println("App running")
+	fmt.Println("Dometika listening")
+	r := gin.Default()
+	r.GET("/ping", domestikaListHandler)
+	r.POST("/recipes", newDomestikHandler)
+	r.PUT("/recipes/:id", UpdateDomestikaHandler)
+	r.DELETE("/recipes/:id", DeleteRecipeHandler)
+	r.GET("/recipes/search", SearchDomestikHandler)
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
