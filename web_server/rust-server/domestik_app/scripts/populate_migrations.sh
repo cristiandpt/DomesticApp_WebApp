581da7f5e527fee36c672e7cec2db18c68af76f4
Nomka0 <juanjose111102@gmail.com>
2023-11-28 00:59:37 -0500

    if condition added to populate script

--- web_server/rust-server/domestik_app/scripts/populate_migrations.sh
+++ web_server/rust-server/domestik_app/scripts/populate_migrations.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
+cd ..
 echo "DATABASE_URL=${DATABASE_URL}" > .env
 
-diesel setup
-
-diesel migration generate data 
-
-
-# populating up.sql
 TIMESTAMP_DATA=$(ls migrations | grep data)
-cat tables_definition.sql > "migrations/${TIMESTAMP_DATA}/up.sql"
+if [ -d "migrations/${TIMESTAMP_DATA}/" ]; then
+ # If there's no initial data folder
+ echo "Directory exists, so diesel setup is not performed" 
+else 
+  diesel setup
+  diesel migration generate data 
+  # populating up.sql
+  cat tables_definition.sql > "migrations/${TIMESTAMP_DATA}/up.sql"
 
-# dropping tables in down.sql
-grep "TABLE" tables_definition.sql | sed 's/ (//g' | sed 's/CREATE/DROP/' | awk '{print $0";"}' > "migrations/${TIMESTAMP_DATA}/down.sql"
+  # dropping tables in down.sql
+  grep "TABLE" tables_definition.sql | sed 's/ (//g' | sed 's/CREATE/DROP/' | awk '{print $0";"}' > "migrations/${TIMESTAMP_DATA}/down.sql"
+  
+  # migrate tables to postgres container
+  diesel migration run
+fi
 
-# migrate tables to postgres container
-diesel migration run
+
