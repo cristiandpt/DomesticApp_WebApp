3ece7267fba86e21cdd1db4c990e72395d1080ed
Nomka0 <juanjose111102@gmail.com>
2023-11-28 23:00:30 -0500

    [FIX] added tac command section to populate script (to drop tables)

--- web_server/rust-server/domestik_app/populate_migrations.sh
+++ web_server/rust-server/domestik_app/populate_migrations.sh
@@ -15,7 +15,7 @@ else
   cat ./database/db/tables_definition.sql> "migrations/${TIMESTAMP_DATA}/up.sql"
 
   # dropping tables in down.sql
-  grep "TABLE" ./database/db/tables_definition.sql | sed 's/ (//g' | sed 's/CREATE/DROP/' | awk '{print $0";"}' > "migrations/${TIMESTAMP_DATA}/down.sql"
+  grep "TABLE" ./database/db/tables_definition.sql | sed 's/ (//g' | sed 's/CREATE/DROP/' | awk '{print $0";"}' | tac > "migrations/${TIMESTAMP_DATA}/down.sql"
   
   # migrate tables to postgres container
   diesel migration run
