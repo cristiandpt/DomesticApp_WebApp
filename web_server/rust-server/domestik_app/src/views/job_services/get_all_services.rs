3eb81fd50318862696c47f02d2996135ff9516b1
cristian_david_gentoo <cristian.pacheco@correounivalle.edu.co>
2023-11-29 13:27:50 -0500

    [MODIFY] The service dnpoint for bd connection

--- web_server/rust-server/domestik_app/src/views/job_services/get_all_services.rs
+++ web_server/rust-server/domestik_app/src/views/job_services/get_all_services.rs
@@ -3,12 +3,7 @@ use actix_web::{web, Responder};
 use serde_json::value::Value;
 use serde_json::Map;
 
-use crate::diesel;
-use diesel::prelude::*;
-
-use crate::infraestructure::database::database_connection::DB;
-
-pub async fn get_services(db: DB) -> impl Responder {
+pub async fn get_services() -> impl Responder {
     let services: Map<String, Value> = json_file_reader::read_file("src/mock-data/services.json");
     web::Json(services)
 }
