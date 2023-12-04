860223780208a1cfe9a019a196416ba993776f5e
leandroace <60167865+leandroace@users.noreply.github.com>
2023-12-04 11:34:43 -0500

    UPDATE ADD FIX
    Rate and cvv do not go in wokerJob
    change userphone to workerphone in workerjob, also change the foreign key
    change userphone to workerphone in adress also change the foreign key
    change userphone to workerphone in service, also change the foreign key
    add to rating workerphone with its respective foreign key and the constraint
    remove workerrating
    in payment change userinfo to client_phone
    in payment_method change userinfo to client_phone
    move client below userinfo
    all foreign keys put not null

--- database/db/tables_definition.sql
+++ database/db/tables_definition.sql
@@ -8,11 +8,16 @@ CREATE TABLE User_info (
     user_type VARCHAR(100)
 );
 
+CREATE TABLE Client (
+    phone VARCHAR(11) PRIMARY KEY,
+    services_url VARCHAR(255)
+);
+
 CREATE TABLE Address (
     address_loc VARCHAR(100),
-    user_phone VARCHAR(11) NOT NULL,
-    CONSTRAINT fk_address_user_info FOREIGN KEY (user_phone) REFERENCES User_info(user_phone),
-    PRIMARY KEY (address_loc, user_phone)
+    worker_phone VARCHAR(11) NOT NULL,
+    CONSTRAINT fk_address_worker FOREIGN KEY (worker_phone) REFERENCES Worker(phone),
+    PRIMARY KEY (address_loc, worker_phone)
 );
 
 CREATE TABLE Worker (
@@ -29,32 +34,30 @@ CREATE TABLE Job (
 
 CREATE TABLE Job_Type (
     id SERIAL PRIMARY KEY,
-    hour_periodicity INT,
+    hour_periodicity TIME,
     periodicity_description VARCHAR(100)
 );
 
 CREATE TABLE Worker_job (
-    user_phone VARCHAR(11) NOT NULL,
+    worker_phone VARCHAR(11) NOT NULL,
     jobId SERIAL NOT NULL,
     price DOUBLE PRECISION CONSTRAINT chk_positive_price CHECK (price >= 0),
     laborType VARCHAR(100),
-    job_type_id INT,
-    PRIMARY KEY (user_phone, jobId),
-    CONSTRAINT fk_worker_job_user_info FOREIGN KEY (user_phone) REFERENCES User_info(user_phone),
+    job_type_id INT NOT NULL,
+    PRIMARY KEY (worker_phone, jobId),
+    CONSTRAINT fk_worker_job_worker FOREIGN KEY (worker_phone) REFERENCES Worker(phone),
     CONSTRAINT fk_worker_job_job FOREIGN KEY (jobId) REFERENCES Job(jobId),
-    CONSTRAINT fk_worker_job_job_type FOREIGN KEY (job_type_id) REFERENCES Job_Type(id),
-    CONSTRAINT chk_positive_rate CHECK (rate >= 0 AND rate <= 5), -- Adding name to the rate constraint
-    CONSTRAINT chk_positive_cvv CHECK (cvv >= 0) -- Adding name to the cvv constraint
+    CONSTRAINT fk_worker_job_job_type FOREIGN KEY (job_type_id) REFERENCES Job_Type(id)
 );
 
 CREATE TABLE Service (
     serviceId SERIAL PRIMARY KEY,
     description VARCHAR(100),
     status VARCHAR(100),
-    client_phone VARCHAR(11),
-    worker_phone VARCHAR(11),
-    jobId INT,
-    CONSTRAINT fk_service_client FOREIGN KEY (client_phone) REFERENCES User_info(user_phone),
+    client_phone VARCHAR(11) NOT NULL,
+    worker_phone VARCHAR(11) NOT NULL,
+    jobId INT NOT NULL,
+    CONSTRAINT fk_service_client FOREIGN KEY (client_phone) REFERENCES Client(phone),
     CONSTRAINT fk_service_worker FOREIGN KEY (worker_phone) REFERENCES Worker(phone),
     CONSTRAINT fk_service_job FOREIGN KEY (jobId) REFERENCES Job(jobId)
 );
@@ -62,22 +65,16 @@ CREATE TABLE Service (
 CREATE TABLE Rating (
     ratingId SERIAL PRIMARY KEY,
     rate FLOAT CHECK (rate >= 0 AND rate <= 5),
-    serviceId INT,
-    CONSTRAINT fk_rating_service FOREIGN KEY (serviceId) REFERENCES Service(serviceId)
-);
-
-CREATE TABLE Worker_rating (
-    id SERIAL PRIMARY KEY,
-    user_phone VARCHAR(11) NOT NULL,
-    ratingId INT,
-    CONSTRAINT fk_worker_rating_user_info FOREIGN KEY (user_phone) REFERENCES User_info(user_phone),
-    CONSTRAINT fk_worker_rating_rating FOREIGN KEY (ratingId) REFERENCES Rating(ratingId)
+    serviceId INT NOT NULL,
+    worker_phone VARCHAR(11) NOT NULL,
+    CONSTRAINT fk_rating_service FOREIGN KEY (serviceId) REFERENCES Service(serviceId),
+    CONSTRAINT fk_rating_worker FOREIGN KEY (worker_phone) REFERENCES Worker(phone)
 );
 
 CREATE TABLE Invoice (
     invoiceId SERIAL PRIMARY KEY,
     total DOUBLE PRECISION CONSTRAINT chk_positive_total CHECK (total >= 0),
-    serviceId INT,
+    serviceId INT NOT NULL,
     CONSTRAINT fk_invoice_service FOREIGN KEY (serviceId) REFERENCES Service(serviceId)
 );
 
@@ -85,8 +82,8 @@ CREATE TABLE Notification (
     notificationId SERIAL PRIMARY KEY,
     notificationDate DATE,
     notificationTime TIME,
-    worker_phone VARCHAR(11),
-    serviceId INT,
+    worker_phone VARCHAR(11) NOT NULL,
+    serviceId INT NOT NULL,
     CONSTRAINT fk_notification_worker FOREIGN KEY (worker_phone) REFERENCES Worker(phone),
     CONSTRAINT fk_notification_service FOREIGN KEY (serviceId) REFERENCES Service(serviceId)
 );
@@ -94,15 +91,10 @@ CREATE TABLE Notification (
 CREATE TABLE Payment (
     paymentId SERIAL PRIMARY KEY,
     paymentDate DATE,
-    invoiceId INT,
-    client_phone VARCHAR(11),
+    invoiceId INT NOT NULL,
+    client_phone VARCHAR(11) NOT NULL,
     CONSTRAINT fk_payment_invoice FOREIGN KEY (invoiceId) REFERENCES Invoice(invoiceId),
-    CONSTRAINT fk_payment_client FOREIGN KEY (client_phone) REFERENCES User_info(user_phone)
-);
-
-CREATE TABLE Client (
-    phone VARCHAR(11) PRIMARY KEY,
-    services_url VARCHAR(255)
+    CONSTRAINT fk_payment_client FOREIGN KEY (client_phone) REFERENCES Client(phone)
 );
 
 CREATE TABLE Payment_method (
@@ -112,7 +104,7 @@ CREATE TABLE Payment_method (
     card_number VARCHAR(100),
     expiration_date DATE,
     client_phone VARCHAR(11) NOT NULL,
-    CONSTRAINT fk_payment_method_client FOREIGN KEY (client_phone) REFERENCES User_info(user_phone)
+    CONSTRAINT fk_payment_method_client FOREIGN KEY (client_phone) REFERENCES Client(phone)
 );
 
 CREATE TABLE Login (
@@ -123,10 +115,6 @@ CREATE TABLE Login (
     refresh_token VARCHAR(100),
     refresh_token_expiration_date DATE,
     access_token_expiration_date DATE,
-    user_phone VARCHAR(11),
+    user_phone VARCHAR(11) NOT NULL,
     CONSTRAINT fk_login_user_info FOREIGN KEY (user_phone) REFERENCES User_info(user_phone)
 );
-
-
-
-
