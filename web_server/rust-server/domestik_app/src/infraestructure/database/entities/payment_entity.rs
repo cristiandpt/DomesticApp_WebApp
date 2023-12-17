24276fe175d85c9e7941bfff048dc46d39f0a614
cristian_david_gentoo <cristian.pacheco@correounivalle.edu.co>
2023-12-17 17:14:50 -0500

    Fix on payment service

--- web_server/rust-server/domestik_app/src/infraestructure/database/entities/payment_entity.rs
+++ web_server/rust-server/domestik_app/src/infraestructure/database/entities/payment_entity.rs
@@ -5,7 +5,6 @@ use chrono::NaiveDate;
 
 #[derive(Queryable, Insertable, Clone, Debug)]
 #[table_name="payment"]
-<<<<<<< HEAD
 pub struct PaymentEntity {
     pub paymentid: i32,
     #[diesel(serialize_as = Option<NaiveDate>)]
@@ -30,14 +29,3 @@ impl PaymentEntity {
         }
     }
 }
-=======
-pub struct payment {
-    pub paymentid : i32,
-    #[diesel(serialize_as = Option<NaiveDate>)]
-    pub paymentdate : Option<NaiveDate>,
-    #[diesel(serialize_as = Option<i32>)]
-    pub invoiceid : Option<i32>,
-    #[diesel(serialize_as = Option<String>)]
-    pub client_phone : Option<String>,
-}
->>>>>>> origin
